083fbce59448cf9480f08f423e20a25e7e16aa03
Abdullah Barakat <abdallah771363885@gmail.com>
2024-05-12 17:56:58 +0300

    Edit HeroSection

--- src/views/HomeView/index.vue
+++ src/views/HomeView/index.vue
@@ -5,16 +5,20 @@ import ServicesSection from "./components/ServicesSection.vue";
 </script>
 
 <template>
+  <div class="absolute bg-[#0a001f] w-full h-screen">
+    <img class="mix-blend-screen	saturate-20	" src="/assets/photos/hero-bg.svg">
+    <img class="absolute w-100 -ml-40 -mb-50 left-0 bottom-0 " src="/assets/photos/planet.png" >
+  </div>
   <div
     aria-hidden="true"
     class="absolute h-screen justify-between items-center inset-0 grid grid-cols-2 -space-x-52 z-0"
   >
     <div
-      class="blur-[150px] mt-auto w-1/4 h-1/2 bg-gradient-to-r from-[#4833cc] to-[rgb(176,28,255)] opacity-40"
+      class="blur-[150px] -ms-10 mt-auto w-2/6 h-1/3 bg-gradient-to-r from-[#4833cc] to-[rgb(176,28,255)] opacity-80"
     ></div>
     <div class="w-full h-full flex items-center justify-end">
       <div
-        class="blur-[150px] w-1/3 h-1/2 bg-gradient-to-r from-[#4833cc] to-[rgb(176,28,255)] opacity-20"
+        class="blur-[150px] w-1/3 h-1/2 bg-gradient-to-r from-[#4833cc] to-[rgb(176,28,255)] opacity-90"
       ></div>
     </div>
   </div>
